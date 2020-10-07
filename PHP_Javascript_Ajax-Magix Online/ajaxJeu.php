e3b3ee8b7d4f860f15fbc05828118d173d0faf3d
emmakarenine <makraphone@hotmail.com>
2020-10-07 13:07:01 -0400

    cleaning

--- PHP_Javascript_Ajax-Magix Online/ajaxJeu.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once("action/AjaxJeuAction.php");
-
-	$action = new AjaxJeuAction();
-	$data = $action->execute();
-
-	echo json_encode($data["result"]);
--- PHP_Javascript_Ajax-Magix Online/ajaxState.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once("action/AjaxStateAction.php");
-
-	$action = new AjaxStateAction();
-	$data = $action->execute();
-
-	echo json_encode($data["result"]);
